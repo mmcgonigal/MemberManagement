3b11609c184fe5bcd34bd97297064c39b7ff0d3e
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-16 16:49:46 -0500

    member service for enrollment

---
+++ src/main/java/helloSpring/service/MemberService.java
@@ -0,0 +1,26 @@
+package helloSpring.service;
+
+import helloSpring.domain.Member;
+import helloSpring.repository.MemberRepository;
+import helloSpring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+//    enrollment
+
+    public Long join(Member member) {
+//        duplicated name can not be joined .
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m ->{
+            throw new IllegalStateException( "Already existing member") ;
+        });
+
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+}
