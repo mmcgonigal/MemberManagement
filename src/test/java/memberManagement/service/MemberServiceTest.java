ed4d18170d40035b7602ede1f5cac60767a10f58
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-16 17:48:14 -0500

    Test join

--- src/test/java/memberManagement/service/MemberServiceTest.java
+++ src/test/java/memberManagement/service/MemberServiceTest.java
@@ -1,11 +1,28 @@
 package memberManagement.service;
 
+import memberManagement.domain.Member;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MemberServiceTest {
 
+    MemberService memberService = new MemberService();
+
     @Test
     void join() {
+        //given   --- when in this condition , when it is given
+
+        Member member = new Member();
+        member.setName("Hello");
+
+        //when    --- when it is executed
+
+        Long saveId = memberService.join(member);
+
+        //then    --- result should be this .
+        Member foundMember  = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(foundMember.getName());
     }
 
     @Test
