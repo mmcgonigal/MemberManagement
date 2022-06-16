d329759ebecbf0828dab3afa01daa1bce9880206
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-16 17:06:17 -0500

    member emrollment

--- src/main/java/helloSpring/service/MemberService.java
+++ src/main/java/helloSpring/service/MemberService.java
@@ -40,6 +40,10 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
 
 
 
