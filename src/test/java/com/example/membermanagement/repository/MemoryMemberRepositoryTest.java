707ec339f52ecd06831a97cf4980fed6eaa1e10b
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-15 21:24:03 -0500

    @Test save()

--- src/test/java/com/example/membermanagement/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/example/membermanagement/repository/MemoryMemberRepositoryTest.java
@@ -1,2 +1,23 @@
-package com.example.membermanagement.repository;public class MemoryMemberRepositoryTest {
+package com.example.membermanagement.repository;
+
+import helloSpring.domain.Member;
+import helloSpring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("SpringTest");
+        repository.save(member);
+
+        Member result = repository.findbyId(member.getId()).get();
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+
+
 }
