824efae37ca0c0c709eefdb38decd4eb2650a9a7
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-18 00:12:19 -0500

    config

--- src/main/java/com/example/membermanagement/ManagementConfig.java
+++ src/main/java/com/example/membermanagement/ManagementConfig.java
@@ -1,5 +1,7 @@
 package com.example.membermanagement;
 
+import memberManagement.repository.MemberRepository;
+import memberManagement.repository.MemoryMemberRepository;
 import memberManagement.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,18 @@ public class ManagementConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService();
+        return new MemberService(memberRepository() );
     }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+
+
+
+
+
+
 }
