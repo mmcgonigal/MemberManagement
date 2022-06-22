e55913a646ee009e276cd3a1cc2e91afa6090d62
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-06-21 23:34:52 -0500

    controller create form for new member

--- src/main/java/com/example/membermanagement/controller/MemberController.java
+++ src/main/java/com/example/membermanagement/controller/MemberController.java
@@ -4,6 +4,7 @@ package com.example.membermanagement.controller;
 import com.example.membermanagement.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -13,5 +14,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }
